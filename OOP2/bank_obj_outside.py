2f47855c9b1dd873c772f4a53b53cb18d3d08e9b
pattanasin09 <Pattanasin9@hotmail.com>
2022-06-21 13:57:07 +0700

    update

---
+++ OOP2/bank_obj_outside.py
@@ -0,0 +1,20 @@
+#this file is inside bank package
+#call module
+#from customer import Customer
+#from account import Account
+#from bank import customer,account
+from bank.customer import Customer
+from bank.account import Account
+
+
+cus1 = Customer()
+cus1.new_customer()
+print(cus1.name)
+
+cus1_acc = Account
+cus1_acc.open_account('ttff','Saving','10-123-456',500)
+
+print('**** Open Bank Account Detail ****')
+print(cus1.cus_info())
+print(cus1_acc.display_balance())
+#print(cus1.cus_info())
